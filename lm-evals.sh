070f55cb9e1e5a068ffc134f15555325de2cc062
Chad Phillips <chad@apartmentlines.com>
2025-03-15 11:02:08 -0400

    add lm-eval scripts for phi-4 and qwen-2.5-7b

--- lm-evals.sh
+++ lm-evals.sh
@@ -48,3 +48,37 @@ lm-eval --model hf \
   --batch_size 8 \
   --output_path ./results-fine-tune-mistral-7b \
   --log_samples
+
+# Phi-4
+lm-eval --model hf \
+  --model_args pretrained=unsloth/phi-4-unsloth-bnb-4bit,dtype=float16 \
+  --tasks hellaswag \
+  --device cuda:0 \
+  --batch_size 8 \
+  --output_path ./results-phi-4 \
+  --log_samples
+
+lm-eval --model hf \
+  --model_args pretrained=unsloth/phi-4-unsloth-bnb-4bit,peft=/home/hunmonk/git/raspberry/outputs/phi-4/checkpoint-177,dtype=float16 \
+  --tasks hellaswag \
+  --device cuda:0 \
+  --batch_size 8 \
+  --output_path ./results-fine-tune-phi-4 \
+  --log_samples
+
+# Qwen-2.5 7B
+lm-eval --model hf \
+  --model_args pretrained=unsloth/Qwen2.5-7B,dtype=float16 \
+  --tasks hellaswag \
+  --device cuda:0 \
+  --batch_size 8 \
+  --output_path ./results-qwen-2.5-7b \
+  --log_samples
+
+lm-eval --model hf \
+  --model_args pretrained=unsloth/Qwen2.5-7B,peft=/home/hunmonk/git/raspberry/outputs/qwen-2.5-7b/checkpoint-177,dtype=float16 \
+  --tasks hellaswag \
+  --device cuda:0 \
+  --batch_size 8 \
+  --output_path ./results-fine-tune-qwen-2.5-7b \
+  --log_samples
