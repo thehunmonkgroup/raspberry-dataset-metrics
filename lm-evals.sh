003021236ae0da634fc022a080829db21a1c3ddd
Chad Phillips <chad@apartmentlines.com>
2025-05-10 19:40:47 -0400

    gms8k task

--- lm-evals.sh
+++ lm-evals.sh
@@ -1,7 +1,7 @@
 # Llama-3.2-3B
 lm-eval --model hf \
   --model_args pretrained=meta-llama/Llama-3.2-3B-Instruct,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-llama-3.2-3b \
@@ -9,7 +9,7 @@ lm-eval --model hf \
 
 lm-eval --model hf \
   --model_args pretrained=meta-llama/Llama-3.2-3B-Instruct,peft=$(pwd)/outputs/llama-3.2-3b,tokenizer=$(pwd)/outputs/llama-3.2-3b,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-fine-tune-llama-3.2-3b \
@@ -18,7 +18,7 @@ lm-eval --model hf \
 # Llama-3.1-8B
 lm-eval --model hf \
   --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-llama-3.1-8b \
@@ -26,7 +26,7 @@ lm-eval --model hf \
 
 lm-eval --model hf \
   --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,peft=$(pwd)/outputs/llama-3.1-8b,tokenizer=$(pwd)/outputs/llama-3.1-8b,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-fine-tune-llama-3.1-8b \
@@ -35,7 +35,7 @@ lm-eval --model hf \
 # Mistral 7B
 lm-eval --model hf \
   --model_args pretrained=mistralai/Mistral-7B-Instruct-v0.3,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-mistral-7b \
@@ -43,7 +43,7 @@ lm-eval --model hf \
 
 lm-eval --model hf \
   --model_args pretrained=unsloth/Mistral-7B-Instruct-v0.3,peft=$(pwd)/outputs/mistral-7b,tokenizer=$(pwd)/outputs/mistral-7b,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-fine-tune-mistral-7b \
@@ -52,7 +52,7 @@ lm-eval --model hf \
 # Phi-4
 lm-eval --model hf \
   --model_args pretrained=microsoft/phi-4,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-phi-4 \
@@ -60,7 +60,7 @@ lm-eval --model hf \
 
 lm-eval --model hf \
   --model_args pretrained=microsoft/phi-4,peft=$(pwd)/outputs/phi-4,tokenizer=$(pwd)/outputs/phi-4,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-fine-tune-phi-4 \
@@ -69,7 +69,7 @@ lm-eval --model hf \
 # Qwen-2.5 7B
 lm-eval --model hf \
   --model_args pretrained=Qwen/Qwen2.5-7B-Instruct,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-qwen-2.5-7b \
@@ -77,7 +77,7 @@ lm-eval --model hf \
 
 lm-eval --model hf \
   --model_args pretrained=Qwen/Qwen2.5-7B-Instruct,peft=$(pwd)/outputs/qwen-2.5-7b,tokenizer=$(pwd)/outputs/qwen-2.5-7b,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-fine-tune-qwen-2.5-7b \
@@ -86,7 +86,7 @@ lm-eval --model hf \
 # Gemma 3
 lm-eval --model hf \
   --model_args pretrained=google/gemma-3-4b-it,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-gemma-3-4b-it \
@@ -94,7 +94,7 @@ lm-eval --model hf \
 
 lm-eval --model hf \
   --model_args pretrained=google/gemma-3-4b-it,peft=$(pwd)/outputs/gemma-3-4b-it,tokenizer=$(pwd)/outputs/gemma-3-4b-it,dtype=bfloat16 \
-  --tasks hellaswag \
+  --tasks gsm8k \
   --device cuda:0 \
   --batch_size 8 \
   --output_path ./results-fine-tune-gemma-3-4b-it \
